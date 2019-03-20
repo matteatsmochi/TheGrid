81fc749b6d6f189e24b4de439a3e069f0d3ac212
Dylan Meville <dylan@dylanmeville.com>
2019-03-20 12:13:20 -0600

    Test Test
    Test Test

--- Assets/Scripts/DrawLine.cs
+++ Assets/Scripts/DrawLine.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DrawLine : MonoBehaviour
 {
     private LineRenderer lineRenderer;
-    private float counter;
+    public float counter;
     private float dist;
     private int currentWaypoint = 0;
     private bool drawing;
@@ -25,27 +25,45 @@ public class DrawLine : MonoBehaviour
         lineRenderer.endWidth = 0.15f;
     }
 
-    
+
 
     void Update()
     {
+        //testing to make sure all the waypoints can be drawn. Works!
+        //if(drawing) {
+        //    lineRenderer.SetPosition(0, waypoints[0]);
+        //    for(int i = 0; i < waypoints.Count; i++) {
+        //        lineRenderer.positionCount++;
+        //        Vector3 p = waypoints[i];
+        //        lineRenderer.SetPosition(i, p);
+        //    }
+        //}
+        //return;
+
         if (drawing)
         {
-            if (counter <= dist)
-            {
-                counter += (lineDrawSpeed) * Time.deltaTime;
-                lineRenderer.SetPosition(currentWaypoint, Vector3.Lerp(waypoints[currentWaypoint], waypoints[currentWaypoint + 1], counter));
+            counter += (lineDrawSpeed) * Time.deltaTime;
+
+            if(counter <= dist)
+            {   //counter starts at 0, so this is block is evaluated right off the start
+                lineRenderer.SetPosition(currentWaypoint+1, Vector3.Lerp(waypoints[currentWaypoint], waypoints[currentWaypoint + 1], counter/dist)); //lerp works on [0,1], counter is [0,dist] so you have to normalize it
+                //currentWaypoint+1, we offset by 1 because you already have currentWaypoint (0) set on the first iteration
+
                 front.transform.position = waypoints[currentWaypoint + 1];
-            }
+
+            } 
             else
             {
-                if (waypoints.Count > lineRenderer.positionCount + 1)
+                if(waypoints.Count > lineRenderer.positionCount) 
                 {
                     NextWaypoint();
                     counter = 0;
                     dist = Vector3.Distance(waypoints[currentWaypoint], waypoints[currentWaypoint + 1]);
+                } 
+                else 
+                {
+                    drawing = false;
                 }
-                    else drawing = false;
             }
         }
         
@@ -60,11 +78,14 @@ public class DrawLine : MonoBehaviour
                 waypoints.Add(wp[i].position);
             }
 
-            lineRenderer.positionCount = 1;
+            lineRenderer.positionCount = 1;             //setting the first waypoint immediately once we know where the waypoints are
             lineRenderer.SetPosition(0, waypoints[0]);
+            lineRenderer.positionCount++;
             dist = Vector3.Distance(waypoints[0], waypoints[1]);
             drawing = true;
             populated = true;
+
+
         }
 
     }
@@ -74,14 +95,16 @@ public class DrawLine : MonoBehaviour
         lineRenderer.positionCount++;
 
         currentWaypoint++;
-        
-        if (currentWaypoint == 0)
+        front.transform.position = waypoints[currentWaypoint];
+
+
+        if(currentWaypoint == 0)
         {
-            lineRenderer.SetPosition(currentWaypoint, waypoints[0]);
+            lineRenderer.SetPosition(currentWaypoint+1, waypoints[0]);
         }
         else
         {
-            lineRenderer.SetPosition(currentWaypoint, waypoints[currentWaypoint - 1]);
+            lineRenderer.SetPosition(currentWaypoint+1, waypoints[currentWaypoint - 1]);
         }
     }
 }
